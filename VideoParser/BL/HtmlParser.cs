8ca8bfaceec76baf0bf17192d25f2cb7b8a93f48
anton.sergievskiy <aser@diceus.com>
2016-10-23 14:52:45 +0300

    ready

---
+++ VideoParser/BL/HtmlParser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoParser.BL
+{
+	public static class HtmlParser
+	{
+		public static string GetHtml(string url)
+		{
+			if (string.IsNullOrWhiteSpace(url))
+				throw new Exception("Пустая ссылка");
+
+			if (!url.Contains("http://"))
+				url = "http://" + url;
+
+			string htmlCode = string.Empty;
+			try {
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+				using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+				{
+					if (response.StatusCode == HttpStatusCode.OK)
+					{
+						Stream receiveStream = response.GetResponseStream();
+						StreamReader readStream = null;
+
+						if (response.CharacterSet == null)
+						{
+							readStream = new StreamReader(receiveStream);
+						}
+						else
+						{
+							readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+						}
+
+						htmlCode = readStream.ReadToEnd();
+						readStream.Close();
+					}
+					else
+						throw new Exception("Наверное, неправильная ссылка");
+				}
+			}
+			catch
+			{
+				throw new Exception("Наверное, неправильная ссылка");
+			}
+			
+			return htmlCode;
+		}
+
+		public static string GetFrameUrl(string htmlCode)
+		{
+			int index = htmlCode.IndexOf("http://player.novy.tv/embed");
+			string tmp = htmlCode.Remove(0, index);
+			string result = tmp.Remove(tmp.IndexOf("\""));
+			return result;
+		}
+
+		public static string GetVideoFileUrl(string url)
+		{
+			var html = GetHtml(url);
+			var frameUrl = GetFrameUrl(html);
+			var videoHtml = GetHtml(frameUrl);
+
+			string fileName = "mq.mp4";
+			int index = videoHtml.IndexOf(fileName);
+			string tmp = videoHtml.Remove(index + fileName.Length);
+			string result = tmp.Remove(0, tmp.LastIndexOf("\"") + 1);
+			return result;
+		}
+	}
+}
---
+++ VideoParser/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VideoParser.BL;
+
+namespace VideoParser
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private WebClient client;
+
+		public MainWindow()
+		{
+			client = new WebClient();
+			InitializeComponent();
+		}
+
+		private void getVideoButton_Click(object sender, RoutedEventArgs e)
+		{
+			string url = pageUrlTextBox.Text;
+			try
+			{
+				var videoUrl = HtmlParser.GetVideoFileUrl(url);
+				videoUrlTextBox.Text = videoUrl;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}			
+		}
+
+		private void pageUrlTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if(string.IsNullOrEmpty(pageUrlTextBox.Text))
+				getVideoButton.IsEnabled = false;
+			else
+				getVideoButton.IsEnabled = true;
+		}
+
+		private void videoUrlTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if(!string.IsNullOrWhiteSpace(videoUrlTextBox.Text))
+			{
+				copyLinkButton.IsEnabled = true;
+				openInBrowserButton.IsEnabled = true;
+				downloadButton.IsEnabled = true;
+			}
+		}
+
+		private void copyLinkButton_Click(object sender, RoutedEventArgs e)
+		{
+			Clipboard.SetText(videoUrlTextBox.Text);
+			MessageBox.Show("Текст скопирован:)");
+		}
+
+		private void openInBrowserButton_Click(object sender, RoutedEventArgs e)
+		{
+			Process proc = new Process();
+			proc.StartInfo.FileName = videoUrlTextBox.Text;
+			proc.StartInfo.UseShellExecute = true;
+			proc.Start();
+		}
+
+		private void downloadButton_Click(object sender, RoutedEventArgs e)
+		{
+			startDownload();
+			downloadButton.IsEnabled = false;
+			downloadGrid.Visibility = Visibility.Visible;
+		}
+
+		private void startDownload()
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "Video file (*.mp4)|*.mp4";
+			saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			if (saveFileDialog.ShowDialog() == true)
+			{ 
+				var videoFileUrl = videoUrlTextBox.Text;
+				client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+				client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+				client.DownloadFileAsync(new Uri(videoFileUrl), saveFileDialog.FileName);
+				downloadCancelbutton.IsEnabled = true;
+			}
+		}
+		void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+		{
+			double megabytesIn = double.Parse(e.BytesReceived.ToString()) / 1048576.00;
+			double totalmegabytes = double.Parse(e.TotalBytesToReceive.ToString()) / 1048576;
+			var percentage = decimal.Parse(e.ProgressPercentage.ToString());
+			downloadTotalLabel.Text = $"{megabytesIn.ToString("F1")} of {totalmegabytes.ToString("F0")}Mb";
+			downloadProgressBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+		}
+		void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+		{
+			downloadCancelbutton.IsEnabled = false;
+			downloadButton.IsEnabled = true;
+			downloadButtonTextBlock.IsEnabled = true;
+			if (e.Cancelled)
+			{
+				MessageBox.Show("Загрузка отменена!");
+				downloadTotalLabel.Text = "Отменено";
+			}
+			else
+			{
+				MessageBox.Show("Загрузка успешно завершена!");
+				downloadTotalLabel.Text = "Готово";
+			}
+		}
+
+		private void downloadCancelbutton_Click(object sender, RoutedEventArgs e)
+		{
+			client.CancelAsync();
+		}
+	}
+}
